6000b1ec5eb4d323dcee539ee5a9227182bfeaa5
Aliriza Bagdat <ambagdat@gmail.com>
2024-04-25 00:38:12 +0100

    login form created and exported

--- src/components/loginPage.js
+++ src/components/loginPage.js
@@ -1,13 +1,46 @@
 import React from "react";
-const loginPage =()=>{
+import {Link} from 'react-router-dom';
+import {Form, Button} from 'react-bootstrap';
+
+
+const loginPage = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+
     const handleLogin = () => {
-        localStorage.setItem('loggedIn', 'true');
-        window.location.href = '/search';
+        const storedUsername = localStorage.getItem('username');
+        const storedPassword = localStorage.getItem('password');
+
+        if (username === storedUsername && password === storedPassword) {
+            localStorage.setItem('loggedIn', 'true');
+
+            window.localation.href = '/search';
+            } else{
+                alert('Invalid username or Password');
+            }
     };
     return (
         <div>
-            <h1>Login</h1>
-        </div>
-    )
-    
-}
+      <h1>Login</h1>
+      <Form>
+        <Form.Group controlId="formBasicUsername">
+          <Form.Label>Username</Form.Label>
+          <Form.Control type="text" placeholder="Enter username" />
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Enter password" />
+        </Form.Group>
+
+        <Button variant="primary" onClick={handleLogin}>
+          Login
+        </Button>
+      </Form>
+      <p>Don't have an account? <Link to="/signup">Signup</Link></p>
+    </div>
+  );
+};
+
+export default LoginPage;
